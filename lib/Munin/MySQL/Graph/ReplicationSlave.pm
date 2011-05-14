e3bad92b21328757e55c6c4dace1328a6a291dc1
Kjell-Magne Øierud <kjellm@acm.org>
2011-05-14 10:15:33 +0200

    Cleaned up the graph files a bit

--- lib/Munin/MySQL/Graph/ReplicationSlave.pm
+++ lib/Munin/MySQL/Graph/ReplicationSlave.pm
@@ -3,39 +3,36 @@ package Munin::MySQL::Graph::ReplicationSlave;
 use warnings;
 use strict;
 
-my %graphs = ();
-
-$graphs{replication} = {
-    config => {
-        global_attrs => {
-            title  => 'Replication',
-            vlabel => 'Activity',
+sub graphs { return {
+    replication => {
+        config => {
+            global_attrs => {
+                title  => 'Replication',
+                vlabel => 'Activity',
+            },
+            data_source_attrs => {
+                draw => 'LINE1',
+            },
         },
-        data_source_attrs => {
-            draw => 'LINE1',
-        },
+        data_sources => [
+            {name => 'slave_running',              label => 'Slave Running',
+                                                   type  => 'GAUGE',
+                                                   draw  => 'AREA',
+                                                   colour=> '00AA00'},
+            {name => 'slave_stopped',              label => 'Slave Stopped',
+                                                   type  => 'GAUGE',
+                                                   draw  => 'AREA',
+                                                   colour=> 'DD0000'},
+            {name => 'Slave_retried_transactions', label => 'Retried Transactions',
+                                                   info  => 'The total number of times since startup that the replication slave SQL thread has retried transactions.'},
+            {name => 'Slave_open_temp_tables',     label => 'Open Temp Tables',
+                                                   info  => 'The number of temporary tables that the slave SQL thread currently has open.'},
+            {name => 'seconds_behind_master',      label => 'Secs Behind Master', 
+                                                   type  => 'GAUGE',
+                                                   info  => 'http://dev.mysql.com/doc/refman/5.1/en/show-slave-status.html'},
+        ],
     },
-    data_sources => [
-        {name => 'slave_running',              label => 'Slave Running',
-                                               type  => 'GAUGE',
-                                               draw  => 'AREA',
-                                               colour=> '00AA00'},
-        {name => 'slave_stopped',              label => 'Slave Stopped',
-                                               type  => 'GAUGE',
-                                               draw  => 'AREA',
-                                               colour=> 'DD0000'},
-        {name => 'Slave_retried_transactions', label => 'Retried Transactions',
-                                               info  => 'The total number of times since startup that the replication slave SQL thread has retried transactions.'},
-        {name => 'Slave_open_temp_tables',     label => 'Open Temp Tables',
-                                               info  => 'The number of temporary tables that the slave SQL thread currently has open.'},
-        {name => 'seconds_behind_master',      label => 'Secs Behind Master', 
-                                               type  => 'GAUGE',
-                                               info  => 'http://dev.mysql.com/doc/refman/5.1/en/show-slave-status.html'},
-    ],
-};
-
-
-sub graphs { return \%graphs; }
+}}
 
 1;
 
